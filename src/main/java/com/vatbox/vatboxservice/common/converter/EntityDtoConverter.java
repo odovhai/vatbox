d6279e8e69f3f8de62adddc0452f5e2b48c5c263
odovhai <odovhai@cybervisiontech.com>
2018-07-23 05:54:32 +0300

    Converter improvements

--- src/main/java/com/vatbox/vatboxservice/common/converter/EntityDtoConverter.java
+++ src/main/java/com/vatbox/vatboxservice/common/converter/EntityDtoConverter.java
@@ -1,12 +1,24 @@
 package com.vatbox.vatboxservice.common.converter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface EntityDtoConverter<E, D> {
 
     D toDto(E source);
-    List<D> toDtoList(List<E> source);
+
     E toEntity(D source);
-    List<E> toEntityList(List<D> source);
+
+    default List<D> toDtoList(List<E> source) {
+        return source.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    default List<E> toEntityList(List<D> source) {
+        return source.stream()
+                .map(this::toEntity)
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/vatbox/vatboxservice/common/converter/impl/DefaultDozerEntityDtoConverter.java
+++ src/main/java/com/vatbox/vatboxservice/common/converter/impl/DefaultDozerEntityDtoConverter.java
@@ -4,9 +4,6 @@ import com.vatbox.vatboxservice.common.converter.EntityDtoConverter;
 import lombok.AllArgsConstructor;
 import org.dozer.DozerBeanMapper;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @AllArgsConstructor
 public class DefaultDozerEntityDtoConverter<E, D> implements EntityDtoConverter<E, D> {
 
@@ -21,22 +18,8 @@ public class DefaultDozerEntityDtoConverter<E, D> implements EntityDtoConverter<
     }
 
     @Override
-    public List<D> toDtoList(List<E> source) {
-        return source.stream()
-                .map(this::toDto)
-                .collect(Collectors.toList());
-    }
-
-    @Override
     public E toEntity(D source) {
         return MAPPER.map(source, entityClass);
-    }
-
-    @Override
-    public List<E> toEntityList(List<D> source) {
-        return source.stream()
-                .map(this::toEntity)
-                .collect(Collectors.toList());
     }
 
 }
